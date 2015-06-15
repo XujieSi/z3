cd31df5429863457ce4b8118b017aabe9f2b725e
Xujie Si <xujie@Xujies-MacBook-Pro.local>
2015-06-14 20:02:04 -0700

    Change type of actual return object of ASTVector.get() from AST to Expr.

--- src/api/java/ASTVector.java
+++ src/api/java/ASTVector.java
@@ -41,7 +41,7 @@ public class ASTVector extends Z3Object
      **/
     public AST get(int i)
     {
-        return new AST(getContext(), Native.astVectorGet(getContext().nCtx(),
+        return new Expr(getContext(), Native.astVectorGet(getContext().nCtx(),
                 getNativeObject(), i));
     }
 
